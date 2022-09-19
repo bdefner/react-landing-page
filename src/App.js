21f3f21dbb5ff0b9a6927bd75e035d92c7f2f617
Beppino Defner <hallo@deona.at>
2022-09-19 19:16:09 +0200

    Create hero section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,142 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
+const navBarStyles = css`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 32px 44px 32px 44px;
+  transition: all 0.5s ease;
+
+  nav {
+    display: flex;
+    gap: 8px;
+  }
+
+  button:hover {
+    background: var(--gradient-discrete);
+  }
+
+  button:last-child {
+    background: var(--gradient1);
+
+    &:hover {
+      background: var(--gradient2);
+    }
+  }
+`;
+
+const heroSectionStyles = css`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: center;
+
+  h1 {
+    font-size: 100px;
+    font-family: var(--display-font-family);
+    font-weight: 900;
+    text-align: center;
+    background: var(--gradient-text1);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    margin: 23px 0 23px 0;
+    line-height: 1em;
+  }
+
+  @media (max-width: 1000px) {
+    h1 {
+      font-size: 10vw;
+    }
+  }
+
+  @media (max-width: 400px) {
+    h1 {
+      font-size: 40px;
+    }
+  }
+
+  h2 {
+    font-size: var(--font-size2);
+    font-weight: bold;
+    max-width: 700px;
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
+  .more-info-wrap {
+    margin-top: 40px;
+    max-width: 600px;
+    text-align: center;
+  }
+  .more-info-wrap > a {
+    color: var(--blue);
+  }
+`;
+
+const buttonStyles = css`
+  font-size: 17px;
+  font-weight: bold;
+  color: white;
+  padding: 0.5em 1.5em;
+  border: solid 0px;
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0);
+  cursor: pointer;
+  transition: all 0.5 ease;
+
+  div {
+    padding: 0.5em 1.5em;
+    border: solid 0px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  .highlight {
+    background: var(--gradient1);
+  }
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div css={navBarStyles}>
+        <div>
+          <a href="index.html">
+            <img src="./public/logo.png" alt="Clover Logo" />
+            <img src="./public/logo-text.png" alt="Clover logo text" />
+          </a>
+        </div>
+        <nav>
+          <button css={buttonStyles}>Pricing</button>
+          <button css={buttonStyles}>Log in</button>
+          <button css={buttonStyles}>Get Started</button>
+        </nav>
+      </div>
+      <main>
+        <section css={heroSectionStyles}>
+          <h1>
+            Think better.
+            <br />
+            Achieve more.
+          </h1>
+          <h2>
+            Clover is a digital notebook that blends notes, tasks, whiteboards,
+            and a daily planner into one streamlined app.
+          </h2>
+          <a css={buttonStyles}>
+            <div className="highlight">Get started for Free</div>
+          </a>
+          <div className="more-info-wrap">
+            <a>Product Hunt's Tool of the Year (Design).</a>
+            <p>Available on Mac, Windows, iPad, iPhone, and the web.</p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,6 +9,75 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        :root {
+          --black: #0e1115;
+          --black-pure: 0, 0, 0;
+          --blue: #6ea4f7;
+          --blue-bright: 106, 54, 255;
+          --cyan: 123, 168, 255;
+          --magenta: 248, 50, 151;
+          --yellow: 255, 239, 94;
+          --link-color: 110, 164, 248;
+          --primary-vertical-section-spacing: 200px;
+          --pink: 249, 65, 197;
+          --orange: 252, 174, 57;
+          --green: 17, 197, 100;
+          --purple: 156, 21, 219;
+          --red: 230, 29, 91;
+          --teal: 9, 186, 198;
+          --light-blue: 17, 198, 255;
+          --grey: 127, 128, 128;
+          --font-family: 'Inter var', -apple-system, BlinkMacSystemFont,
+            'Open Sans', sans-serif;
+          --display-font-family: 'InterDisplay var', -apple-system,
+            BlinkMacSystemFont, 'Open Sans', sans-serif;
+          --gradient-discrete: radial-gradient(
+            150px at 98% 42%,
+            rgba(255, 255, 255, 0.25) 0,
+            rgba(255, 255, 255, 0) 100%
+          );
+          --gradient1: linear-gradient(
+            153deg,
+            rgba(206, 78, 193, 1) 0%,
+            rgba(207, 99, 123, 1) 52%,
+            rgba(206, 189, 107, 1) 100%
+          );
+          --gradient2: linear-gradient(
+            153deg,
+            rgba(24, 120, 107, 1) 0%,
+            rgba(72, 125, 173, 1) 50%,
+            rgba(206, 183, 107, 1) 100%
+          );
+          --gradient-text1: linear-gradient(
+            163deg,
+            rgba(192, 31, 134, 1) 0%,
+            rgba(204, 47, 122, 1) 35%,
+            rgba(244, 97, 83, 1) 56%,
+            rgba(224, 239, 167, 1) 100%
+          );
+          --font-size1: 1rem;
+          --font-size2: 1.4rem;
+          --font-size3: 2rem;
+        }
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+
+        body {
+          margin: 0px;
+          background-color: var(--black);
+          color: white;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          overflow: hidden;
+          font-size: 16px;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
 );
